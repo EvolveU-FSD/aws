07c09354db28cea6615a6a0ed62f6f0842e4e9d7
shaneinceptionu <shane@inceptionu.com>
2025-07-13 23:34:07 -0600

    full run

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end("<h1>I'm Alive!!!</h1>");
+  res.end("<h1>I'm Alive!!! Hello?</h1>");
 });
 
 server.listen(port, () => {
